25f0e9ca4d1579c0d068d70daff35da0fffb4489
AdamMiholjek <s3864779@student.rmit.edu.au>
2020-11-09 20:57:36 +1100

    more

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { ImageBackground, StyleSheet, Text, View, Button, Alert } from "react-native";
+import { ImageBackground, StyleSheet, Text, View, Button, Alert, SafeAreaView } from "react-native";
 
 const App = () => (
-  <View style={styles.container}>
-    <ImageBackground source={require('./data.png')} style={styles.image}>
-      <Text style={styles.text}></Text>
-	  <Button
-	  title= "Log in"
-	  onPress={() => Alert.alert('Logging in...')}
-	  />
-    </ImageBackground>
+<View style={styles.container}>
+<ImageBackground source={require('./data.png')} style={styles.image}>
+  <View>
+      <Text style={styles.text}>Welcome</Text>
   </View>
+	<View style={{flex: 1, alignSelf: "center", position: "absolute"}}>
+		<Button
+			title= "Log in"
+			onPress={() => Alert.alert('Logging in...')}
+		/>
+	</View>
+</ImageBackground>
+</View>
 );
 
 const styles = StyleSheet.create({
@@ -28,7 +32,7 @@ const styles = StyleSheet.create({
     fontSize: 42,
     fontWeight: "bold",
     textAlign: "center",
-    //backgroundColor: "#000000a0"
+    backgroundColor: "#000000a0"
   }
 });
 
