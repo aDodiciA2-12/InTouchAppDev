86eb11e1b96bf2ce233ebab8e694851a2756dc59
AdamMiholjek <s3864779@student.rmit.edu.au>
2020-11-26 22:41:04 +1100

    :/

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import React from "react";
-import { ImageBackground, StyleSheet, Text, View, Button, Alert, SafeAreaView } from "react-native";
+import React, {Component} from 'react';
+import { ImageBackground, StyleSheet, Text, View, Button, Alert, SafeAreaView, AppRegistry, Navigator } from "react-native";
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 const App = () => (
+<NavigationContainer>
 <View style={styles.container}>
 <ImageBackground source={require('./data.png')} style={styles.image}>
-  <View>
-      <Text style={styles.text}>Welcome</Text>
-  </View>
-	<View style={{flex: 1, alignSelf: "center", position: "absolute"}}>
-		<Button
-			title= "Log in"
+	<View style={styles.button2}>
+		<Button 
+			title= "Log In"
 			onPress={() => Alert.alert('Logging in...')}
+		/>
+	</View>
+	<View style={styles.button1}>
+		<Button 
+			title= "Sign Up"
+			onPress={() => Alert.alert('Signing up...')}
 		/>
 	</View>
 </ImageBackground>
 </View>
+</NavigationContainer>
 );
 
 const styles = StyleSheet.create({
@@ -32,7 +40,17 @@ const styles = StyleSheet.create({
     fontSize: 42,
     fontWeight: "bold",
     textAlign: "center",
-    backgroundColor: "#000000a0"
+    //backgroundColor: "#000000a0"
+  },
+  button1: {
+	  flex: 1,
+	  paddingBottom: 100,
+	  justifyContent: "flex-end"
+  },
+  button2: {
+	  flex: 1,
+	  paddingTop: 575,
+	  justifyContent: "flex-end"
   }
 });
 
